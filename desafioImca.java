fea25b450f1c4aa2cd25013f99a34bbffeb7e739
JulietaContursi <contursi11@gmail.com>
2023-02-14 23:43:55 -0300

    progress

--- desafioImca.java
+++ desafioImca.java
@@ -10,7 +10,6 @@ public class desafioImca {
     int maxtam = sc.nextInt();
     int[] vectorPar = new int[maxtam];
     int[] vectorImpar = new int[maxtam];
-    comparacionVectores(vectorPar, vectorImpar);
 
     //recorre ambos vectores cargandolos con numeros aleatorios del 11 al 20 
     Random rnd = new Random();
@@ -19,27 +18,33 @@ public class desafioImca {
       vectorImpar[i] = 11 + rnd.nextInt(10);
     }
 
-    System.out.println(Arrays.toString(vectorPar));
-    System.out.println(Arrays.toString(vectorImpar));
-  }
-
-  ArrayList<Integer> even = new ArrayList<>();
-  ArrayList<Integer> odd = new ArrayList<>();
-
-  public static void comparacionVectores(int[]vectorPar, int []vectorImpar) {
-  int PARES =0;
-  int IMPARES =0;
-  for(int i= 0; i < vectorPar.length; i++){
-    for(int j =0; j< vectorImpar.length; j++){
-    if(vectorPar[i] %2 ==0){
-      if(vectorImpar[j]%2 ==0){
-        IMPARES++;
+    System.out.println("Vector par: " + Arrays.toString(vectorPar));
+    System.out.println("Vector impar: " + Arrays.toString(vectorImpar));
+    
+    for (int i = 0; i < maxtam; i++) {
+      // Si hay un número par en vectorPar, lo deja. Si hay un número impar en vectorPar,
+      // busca si hay un número par en la misma posición en vectorImpar y lo cambia por 0.
+      if (vectorPar[i] % 2 != 0) {
+        for (int j = i; j < maxtam; j++) {
+          if (vectorImpar[j] % 2 == 0) {
+            vectorImpar[j] = 0;
+            break;
+          }
+        }
       }
-      PARES++;
+      // Si hay un número impar en vectorImpar, lo deja. Si hay un número par en vectorImpar,
+      // busca si hay un número impar en la misma posición en vectorPar y lo cambia por 0.
+      if (vectorImpar[i] % 2 == 0) {
+        for (int j = i; j < maxtam; j++) {
+          if (vectorPar[j] % 2 != 0) {
+            vectorPar[j] = 0;
+            break;
+          }
+        }
+      }
     }
+    
+    System.out.println("Vector par con las condiciones: " + Arrays.toString(vectorPar));
+    System.out.println("Vector impar con las condiciones: " + Arrays.toString(vectorImpar));
   }
- }
- System.out.println("en este vector hay tantos numeros pares " + PARES);
-System.out.println("en este vector hay tantos numeros impares " + IMPARES);
-}
 }
