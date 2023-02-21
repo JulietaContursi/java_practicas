b0904feb88f432b55b44a20cd323bf88d44843db
JulietaContursi <contursi11@gmail.com>
2023-02-20 21:43:04 -0300

    changes

---
+++ String/ejercicioBasico.java
@@ -0,0 +1,19 @@
+//Crea una matriz de 4 filas y 2 columnas, y pide al usuario que ingrese los valores para cada elemento de la matriz.
+// Luego, imprime la matriz en la consola
+
+package String;
+
+import java.util.Scanner;
+
+public class ejercicioBasico {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int[][] matriz = new int[4][2];
+
+        //se pide al usuario que ingrese los valores para llenar la matriz
+
+
+
+    }
+}
