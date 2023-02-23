4078a4ae9281f5fe9da594d9d3ab229e5432f337
JulietaContursi <contursi11@gmail.com>
2023-02-23 15:50:39 -0300

    exercise finished positive integers

--- Bucles/enteroPositivoPar.java
+++ Bucles/enteroPositivoPar.java
@@ -1,8 +1,20 @@
 //Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla la secuencia de números pares desde el 2 hasta el número ingresado
-
-
 package Bucles;
 
+import java.util.Scanner;
+
 public class enteroPositivoPar {
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
     
+    System.out.print("Ingrese un numero entero positivo: ");
+    int numero = entrada.nextInt();
+
+    int i = 2; 
+        
+    while (i <= numero) {
+        System.out.print(i + " "); 
+        i += 2; 
+    }
+  }
 }
