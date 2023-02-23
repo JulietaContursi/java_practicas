9e4a0d6e94eb1b4de950106644b42b40d5f3fe07
JulietaContursi <contursi11@gmail.com>
2023-02-23 15:41:04 -0300

    progress

---
+++ Bucles/enteroPositivoPar.java
@@ -0,0 +1,8 @@
+//Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla la secuencia de números pares desde el 2 hasta el número ingresado
+
+
+package Bucles;
+
+public class enteroPositivoPar {
+    
+}
