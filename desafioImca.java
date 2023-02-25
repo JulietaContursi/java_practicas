53b025554db4888b0848afced9c6cc2a7fa7bc6f
JulietaContursi <contursi11@gmail.com>
2023-02-25 17:02:23 -0300

    changes

--- desafioImca.java
+++ desafioImca.java
@@ -6,21 +6,14 @@ public class desafioImca {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        // Pedir al usuario el tamaño de los vectores
         int tamano = pedirTamano(scanner);
-
-        // Crear los vectores y llenarlos con números aleatorios
         int[] pares = crearVector(tamano, random);
         int[] impares = crearVector(tamano, random);
-
-        // Imprimir los vectores
         imprimirVector("PARES: ", pares);
         imprimirVector("IMPARES: ", impares);
 
-        // Comparar los elementos de ambos vectores de manera posicional
         intercambiarValores(pares, impares);
 
-        // Imprimir los vectores intercambiados (si corresponde)
         imprimirVector("PARES: ", pares);
         imprimirVector("IMPARES: ", impares);
     }
@@ -40,8 +33,8 @@ public class desafioImca {
         return vector;
     }
 
-    public static void imprimirVector(String mensaje, int[] vector) {
-        System.out.print(mensaje);
+    public static void imprimirVector(String vectorFinal, int[] vector) {
+        System.out.print(vectorFinal);
         for (int i = 0; i < vector.length; i++) {
             System.out.print(vector[i] + " ,");
         }
@@ -51,17 +44,13 @@ public class desafioImca {
     public static void intercambiarValores(int[] pares, int[] impares) {
         for (int i = 0; i < pares.length; i++) {
             if (pares[i] % 2 == 0 && impares[i] % 2 != 0) {
-                // Si en PARES existe un PAR y en IMPARES un IMPAR, no se hace nada
             } else if (pares[i] % 2 != 0 && impares[i] % 2 == 0) {
-                // Si en PARES existe un IMPAR y en IMPARES un PAR, se intercambian
                 int temp = pares[i];
                 pares[i] = impares[i];
                 impares[i] = temp;
             } else if (pares[i] % 2 == 0 && impares[i] % 2 == 0) {
-                // Si en PARES existe un PAR y en IMPARES un PAR, se coloca 0 en IMPARES
                 impares[i] = 0;
             } else if (pares[i] % 2 != 0 && impares[i] % 2 != 0) {
-                // Si en PARES existe un IMPAR y en IMPARES un IMPAR, se coloca 0 en PARES
                 pares[i] = 0;
             }
         }
